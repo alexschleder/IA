f53bb05b05e9c6f9c6bed2d3d28708101811e3e6
jonasCCa <jonas.ccampos@hotmail.com>
2022-04-24 14:28:12 -0300

    fixed file loading bugs

--- Placeholder.java
+++ Placeholder.java
@@ -4,10 +4,20 @@ import java.io.*;
 public class Placeholder {
 
     public static void main(String args[]) {
-        
+        ArrayList<int[][]> a = loadFile("pares10.txt");
+
+        for(int[][] mat : a) {
+            for(int[] linha : mat) {
+                for(int num : linha) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
     }
 
-    public ArrayList<int[][]> loadFile(String fileName) {
+    public static ArrayList<int[][]> loadFile(String fileName) {
         ArrayList<int[][]> result = new ArrayList<>();
 
         try {
@@ -16,16 +26,21 @@ public class Placeholder {
 
             String linhaAtual = inFile.nextLine();
 
-            int tam = Integer.parseInt(linhaAtual);       
+            int tam = Integer.parseInt(linhaAtual);
+                //System.out.println("tam = " + tam); // Debug
 
             for(int a=0; a<2; a++) {
                 int[][] matrix = new int[tam][tam];
                 for(int i=0; i<tam; i++) {
+                        //System.out.println("linha = " + i); // Debug
                     linhaAtual = inFile.nextLine();
+                        //System.out.println("      = " + linhaAtual); // Debug
                     String[] nums = linhaAtual.split(" ");
 
-                    for(int j=1; j==tam; j++) {
+                    for(int j=1; j<tam+1; j++) {
+                            //System.out.println("coluna = " + (j)); // Debug
                         matrix[i][j-1] = Integer.parseInt(nums[j]) - 1;
+                            //System.out.println("       = " + (Integer.parseInt(nums[j]) - 1)); // Debug
                     }
                 }
                 result.add(matrix);
